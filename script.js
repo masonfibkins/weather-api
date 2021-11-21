a8e817658771509e9a0f27fb4d918d792256ba8c
Mason Fibkins <mason.fibkins@gmail.com>
2021-11-20 20:33:52 -0700

    updated UV index function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var APIKEY = '87875bf0663cfa807f2fc62d46ca41e1';
 
+
  function getWeather(userInput) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKEY)
       .then(function (response) {
@@ -8,26 +9,32 @@ var APIKEY = '87875bf0663cfa807f2fc62d46ca41e1';
       .then(function (data) {
         console.log(data);
         updateWeather(data);
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-            //url to get lat and lon
-            fetch('https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKEY + '&lon=' + lon + '&lat=' + lat)
-            .then(function (response2) {
-              return response2.json();
-            })
-            .then(function (data2) {
-              console.log(data2);
-              document.getElementById('uv').innerHTML = "UV: " + data2.value;
-            })
+        
       });
  }
 
  function updateWeather( d ) {
+  var lat = d.coord.lat;
+  var lon = d.coord.lon;
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
   document.getElementById('name').innerHTML = d.name;
 	document.getElementById('temp').innerHTML = "Temp: " + fahrenheit + '&degF';
   document.getElementById('wind').innerHTML = "Wind: " + d.wind.speed + " MPH";
   document.getElementById('humid').innerHTML = "Humidity: " + d.main.humidity + "%";
+  updateUV(lon, lat);
+}
+
+
+function updateUV(lon, lat){
+      //url to get lat and lon
+      fetch('https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKEY + '&lon=' + lon + '&lat=' + lat)
+      .then(function (response2) {
+        return response2.json();
+      })
+      .then(function (data2) {
+        console.log(data2);
+        document.getElementById('uv').innerHTML = "UV: " + data2.value;
+      });
 }
 
 //event listener to get input from submit button
