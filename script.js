7416e10a6fe0080a7b36d16b1f679f74d8349d15
Mason Fibkins <mason.fibkins@gmail.com>
2021-11-20 17:52:39 -0700

    cleaned up code again

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//temp
-//wind
-//humidity
-//UV
+var APIKEY = '87875bf0663cfa807f2fc62d46ca41e1';
 
  function getWeather(userInput) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=87875bf0663cfa807f2fc62d46ca41e1')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKEY)
       .then(function (response) {
         return response.json();
       })
@@ -14,7 +11,7 @@
         var lat = data.coord.lat;
         var lon = data.coord.lon;
             //url to get lat and lon
-            fetch('https://api.openweathermap.org/data/2.5/uvi?appid=87875bf0663cfa807f2fc62d46ca41e1&lon=' + lon + '&lat=' + lat)
+            fetch('https://api.openweathermap.org/data/2.5/uvi?appid=' + APIKEY + '&lon=' + lon + '&lat=' + lat)
             .then(function (response2) {
               return response2.json();
             })
@@ -23,13 +20,10 @@
               document.getElementById('uv').innerHTML = "UV: " + data2.value;
             })
       });
-      
-
  }
 
  function updateWeather( d ) {
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
-
   document.getElementById('name').innerHTML = d.name;
 	document.getElementById('temp').innerHTML = "Temp: " + fahrenheit + '&degF';
   document.getElementById('wind').innerHTML = "Wind: " + d.wind.speed + " MPH";
