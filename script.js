ce2ac8678b6b073e1dd32c54dbc64e0d5c195fb8
Mason Fibkins <mason.fibkins@gmail.com>
2021-11-22 23:10:24 -0500

    updated forecast function to get 5 day daily weather

--- script.js
+++ script.js
@@ -11,10 +11,9 @@ $(document).ready(function () {
 
   //function to call weather api
   function weatherFunction(searchTerm) {
-
     $.ajax({
       type: "GET",
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + ",US&appid=" + APIKEY,
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=" + APIKEY,
     }).then(function(data){
       
       //clears search box for new input
@@ -24,7 +23,7 @@ $(document).ready(function () {
       var humidityData = data.main.humidity;
       var lon = data.coord.lon;
       var lat = data.coord.lat;
-
+      weatherForecast(lat, lon);
       var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
       
       var card = $("<div>").addClass("card");
@@ -32,14 +31,13 @@ $(document).ready(function () {
       var temp = $("<p>").addClass("Temperature: " + tempData + " °F");
       var wind = $("<p>").addclass("Wind: " + windData + " MPH");
       var humidity = $("<p>").addClass("Humidity: " + humidityData + " %");
-      console.log(data);
       //gets uv index using lat and lon
       $.ajax({
         type: "GET",
         url: "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKEY + "&lat=" + lat + "&lon=" + lon,
       }).then(function(response){
         console.log(response);
-
+        
         var uvColor;
         var uvData = response.value;
         var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
@@ -61,9 +59,39 @@ $(document).ready(function () {
       cardBody.append(title, temp, humidity, wind);
       card.append(cardBody);
       $("#today").append(card);
-      console.log(data);
     }); //end of .then for writing weather data
   } //end of weatherFunction
 
+  //function to get 5 day weather forecast
+  function weatherForecast(lat, lon){
+    $.ajax({
+      type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly&units=imperial&appid=" + APIKEY,
+    }).then(function(data){
+      $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+      for(var i = 1; i < 6; i++){
+        var utcSeconds = data.daily[i].dt;
+        var d = new Date(0); //0 is the key, that sets the date to the epoch
+        d.setUTCSeconds(utcSeconds);
+        var timestamp = d.toLocaleString();
+        var date = timestamp.substring(0,10);
+        var temp = data.daily[i].temp.day;
+        var wind = data.daily[i].wind_speed;  
+        var humidity = data.daily[i].humidity;
+
+        var titleForecast = $("<h3>").addClass("card-title").text(date);
+        
+        var cardForecast = $("<div>").addClass("card")
+        var colForecast = $("<div>").addClass("card bg-primary");
+        var cardBodyForecast = $("<div>").addClass("card-body p-2");
+        var tempForecast = $("<div>").addClass("card-text").text("Temperature: " + temp);
+        var windForecast = $("<div>").addClass("card-text").text("Wind: " + wind);
+        var humidityForecast = $("<div>").addClass("card-text").text("Humidity: " + humidity);
+
+        colForecast.append(cardForecast.append(cardBodyForecast.append(titleForecast, tempForecast, windForecast, humidityForecast)));
+        $("#forecast .row").append(colForecast);
+      }  
+  });
+  }//end of weatherForecast
 }); //end of doc.ready function
 
